84b1fa1c302ee8e3d8d3c14a51b366ac25d615f7
jonnity <jonny1996_ty@yahoo.co.jp>
2023-03-14 22:58:47 +0900

    nextの初期化

---
+++ pages/index.tsx
@@ -0,0 +1,17 @@
+import Head from "next/head";
+
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <title>jonnity&apos;s blog</title>
+        <meta
+          name="description"
+          content="jonnity(@jonnied_man)が運営するブログ"
+        />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <h1>jonnity&apos;s blog</h1>
+    </>
+  );
+}
